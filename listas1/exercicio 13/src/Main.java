273e713e459f0198fccaff017151c7426affd9af
05samuk <samuelribs1308@gmail.com>
2024-02-15 19:34:20 -0300

    exercicio13

--- listas1/exercicio 13/src/Main.java
+++ listas1/exercicio 13/src/Main.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System in = new Scanner(System.in);
-        System.out.println("Digite o raio do circulo: ");
-        double raio = in.();
+    public static void main(String[] args){
+        double area, raio, result;
+        Scanner in = new Scanner(System.in);
+        System.out.println("insira a area do circulo");
+        area = in.nextDouble();
+        raio = Math.PI;
+        System.out.println("o raio do circulo é: " + raio) ;
 
 
     }
